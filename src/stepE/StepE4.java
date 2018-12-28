38f816ac7baafeade88e2300e5dfb8df5cfe8de2
Seungyong Lee <seungyonglee@Seungyongui-MacBook-Pro.local>
2018-12-29 02:06:15 +0900

    fixed

--- src/stepE/StepE4.java
+++ src/stepE/StepE4.java
@@ -14,32 +14,32 @@ public class StepE4 {
 		
 		Scanner s = new Scanner(System.in);
 		System.out.print("구구단의 출력모드(1: 홀수단, 2: 짝수단)입력");
-		this.mode = s.nextInt(); 
+		while(true) {
+		this.mode = s.nextInt();
+		
+		if(this.mode >2)
+			System.out.print("다시입력.\n");
+		else
+			break;
+		}
 	}
 	
 	public void timeTable() {
 		int i,j;
-		if(this.mode == 1) {
-			for(i=3;i<=9;i=i+2) {
+	
+			for(i=this.mode;i<=9;i=i+2) {
 				for(j=1;j<=9;j++) {
+					if(i == 1)
+						i =3;
 					System.out.print(i+" * "+j+" = "+i*j+"	");
+					
 					
 				}
 				System.out.print("\n");
-				System.out.print("\n");
-			}
-		}
-		else {
-			for(i=2;i<=8;i=i+2) {
-				for(j=1;j<=9;j++) {
-					System.out.print(i+" * "+j+" = "+i*j+"	");
-					
-				}
-				System.out.print("\n");
-				System.out.print("\n");
-			}
 			
-		}
+			}
+		
+	
 			
 		
 	}
--- src/stepE/StepE5.java
+++ src/stepE/StepE5.java
@@ -14,34 +14,33 @@ public class StepE5 {
 		
 		Scanner s = new Scanner(System.in);
 		System.out.print("구구단의 출력모드(1: 홀수단, 2: 짝수단)입력");
-		this.mode = s.nextInt(); 
+		while(true) {
+			this.mode = s.nextInt();
+			
+			if(this.mode >2)
+				System.out.print("다시입력.\n");
+			else
+				break;
+			} 
 		System.out.print("한줄에 출력할 갯수 입력");
 		this.column= s.nextInt();
 	}
 	
 	public void timeTable2() {
 		int i,j;
-		if(this.mode == 1) {
-			for(i=3;i<=9;i=i+2) {
+		
+			for(i=this.mode;i<=9;i=i+2) {
 				for(j=1;j<=9;j++) {
+					if(i==1)
+						i=3;
 					System.out.print(i+" * "+j+" = "+i*j+"  ");
 					if(j%this.column==0)
 						System.out.print("\n");
 				}
 				System.out.print("\n");
 			}
-		}
-		else {
-			for(i=2;i<=8;i=i+2) {
-				for(j=1;j<=9;j++) {
-					System.out.print(i+" * "+j+" = "+i*j+"  ");
-					if(j%this.column==0)
-						System.out.print("\n");
-				}
-				System.out.print("\n");
-			}
-			
-		}
+		
+	
 			
 		
 	}
